14d6699feb1e71986f3b04b7c91f5b5f05a232e2
lollobut <lollobut@gmail.com>
2023-10-22 23:40:20 +0200

    verificare errori

---
+++ main.py
@@ -0,0 +1,25 @@
+from flask import Flask, render_template, request, redirect, url_for
+
+app = Flask(__name__)
+
+@app.route('/',methods=['GET'])
+def main():
+    return 'ciao gabriele'
+'''
+    user = {'username': 'Marco'}
+    list = [1,2,3,4,5]
+    return render_template('index.html', title='Home', user=user, list=list)
+'''
+'''
+@app.route('/pippo',methods=['GET'])
+def pippo():
+    #return 'ciao pippo'
+
+    user = {'username': 'Marco'}
+    list = [1,2,3,4,5]
+    return render_template('index.html', title='Home', user=user, list=list)
+'''
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=8080, debug=True)
