c7646cf3a9d2d5566ad656a8e34e21f9070f6928
rinaldigabriele99 <33688641+rinaldigabriele99@users.noreply.github.com>
2023-11-17 16:40:15 +0100

    gabriele main 17-11

--- client_sensors.py
+++ client_sensors.py
@@ -1,14 +1,21 @@
 from requests import get, post
 import time
+import csv
 
 base_url = 'http://localhost:8080'
 #base_url = 'https://mamei-test2-382313.appspot.com'
 
 sensor = 's8'
 
-for i in range(1000):
+for i in range(10):
     print(sensor,'invio....')
-    r = post(f'{base_url}/sensors/{sensor}',data={'val': i})
+    #r = post(f'{base_url}/sensors/{sensor}',data={'val': i})
     time.sleep(1)
+
+
+with open("./mappa_monumenti_italia.csv", newline="", encoding="ISO-8859-1") as filecsv:
+    lettore = csv.reader(filecsv,delimiter=";")
+    header = next(lettore)
+    print(header)
 
 print('done')
--- main.py
+++ main.py
@@ -3,65 +3,75 @@ from flask import Flask, render_template, request, redirect, url_for
 from google.cloud import firestore
 
 app = Flask(__name__)
+local = True
 
 @app.route('/',methods=['GET'])
 def main():
-    #db = firestore.Client() #collego il client col db
     return render_template('index.html', title='Home')
 
+#----------------------------------------------------------------------------------------------
+#LOGIN
 @app.route('/login',methods=['GET'])
 def pippo():
     return render_template('login.html', title='Home')
 
+#-----------------------------------------------------------------------------------------------
+#FIRESTORE SENSORE
+#visualizzo in formato json i dati che manda il client_sensor
+@app.route('/sensors',methods=['GET'])
+def sensor_data():
+    db = firestore.Client.from_service_account_json('credentials.json') if local else firestore.Client()
+    s = []
+    for doc in db.collection('sensors').stream():
+        s.append(doc.id)
+    return json.dumps(s), 200
 
-#aggiungo i valori al firestore che ricevo dal sensore
-@app.route('/upload/<s>',methods=['POST'])
+#aggiungo i valori che ricevo dal sensore al db firestore
+@app.route('/sensors/<s>',methods=['POST'])
 def add_data(s):
+    db = firestore.Client.from_service_account_json('credentials.json') if local else firestore.Client()
     val = float(request.values['val'])
-    db = firestore.Client.from_service_account_json('credentials.json')
-    #db = firestore.Client()
-    doc_ref = db.collection('sensors').document('s')
+    doc_ref= db.collection('sensors').document(s)
     entity = doc_ref.get()
-    if 'values' in entity.to_dict(): 
+    if entity.exists and 'values' in entity.to_dict():
         v = entity.to_dict()['values']
         v.append(val)
         doc_ref.update({'values':v})
     else:
         doc_ref.set({'values':[val]})
-    return 'ok', 200
+    return 'ok',200
 
-@app.route('/getdata/<s>',methods=['GET'])
+#visualizzo i dati presenti sul db firestore
+@app.route('/sensors/<s>',methods=['GET'])
 def get_data(s):
-    db = firestore.Client.from_service_account_json('credentials.json')
-    #db = firestore.Client()
-    doc_ref = db.collection('sensor').document(s)
-    if doc_ref.exists:
-        return json.dumps(doc_ref.get().to_dict()['values']), 200
+    db = firestore.Client.from_service_account_json('credentials.json') if local else firestore.Client()
+    entity = db.collection('sensors').document(s).get()
+    if entity.exists:
+        return json.dumps(entity.to_dict()['values']),200
     else:
-        return 'sensor not found', 404
-    
-@app.route('/getgraph/<s>' ,methods=['GET'])
+        return 'sensor not found',404
+
+#visualizzo un grafico dal coi dati presenti sul db firestore
+@app.route('/graph/<s>',methods=['GET'])
 def graph_data(s):
-    db = firestore.Client.from_service_account_json('credentials.json')
-    #db = firestore.Client()
-    doc_ref = db.collection('sensor').document(s)
-    if doc_ref.exists:
+    db = firestore.Client.from_service_account_json('credentials.json') if local else firestore.Client()
+    entity = db.collection('sensors').document(s).get()
+    if entity.exists:
         d = []
-        d.append(['Number', s])
+        d.append(['Number',s])
         t = 0
-        for x in doc_ref.get().to_dict()['values']:
+        for x in entity.to_dict()['values']:
             d.append([t,x])
-            t += 1
-        return render_template('graph.html', sensor = s, data = json.dumps(d))
+            t+=1
+        return render_template('graph.html',sensor=s,data=json.dumps(d))
     else:
-        return 'sensor not found', 404
-
-
-#mi restituise il contenuto della collection
+        return redirect(url_for('static', filename='sensor404.html'))
+#-----------------------------------------------------------------------------------------------
+#FIRESTORE PERSONE
+#mi restituise il contenuto della collection persone
 @app.route('/fire',methods=['GET'])
 def fire():
-    db = firestore.Client.from_service_account_json('credentials.json')
-    #db = firestore.Client()
+    db = firestore.Client.from_service_account_json('credentials.json') if local else firestore.Client()
     result = ''
     for doc in db.collection('persone').stream():
         result += (f'{doc.id} --> {doc.to_dict()}<br>')
